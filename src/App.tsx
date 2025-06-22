0b36143a8a448b4d57337b9893852af7adc9dccd
Sergey <sergio-kf@mail.ru>
2025-06-22 13:20:47 +0300

    4

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { MainPage } from './pages/MainPage/MainPage';
 import { CoursePage } from './pages/CoursePage/CoursePage';
 import { CurrentCoursePage } from './pages/CurrentCoursePage/CurrentCoursePage';
